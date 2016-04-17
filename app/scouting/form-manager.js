8c3c14dcab488db8a9afe65ae115d39d72a9432a
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-04-17 12:30:50 -0700

    Make misc changes

--- app/scouting/form-manager.js
+++ app/scouting/form-manager.js
@@ -3,35 +3,20 @@ var Datastore = require('nedb');
 var scoutingdb = new Datastore({ filename: __dirname + '/datastore/scouting.db', autoload: true });
 
 function submitScout() {
-	var stage = $("#stage").val();
-	var matchNum = $("#match_num").val();
+	var data = {
+		stage: $("#stage").val(),
+		matchNum: $("#match_num").val(),
+		teamNum: $("#team_num").val(),
+		teamAlliance: $("#team_alliance").val(),
+		autoDefense: $("#auto_defense").val(),
+		autoSpeed: $("#auto_speed").val(),
+		autoHGoals: $("#auto_h_goals").val(),
+		autoLGoals: $("#auto_l_goals").val(),
+		teleopDefense: $("#teleop_defense").val(),
+		teleopSpeed: $("#teleop_speed").val(),
+		teleopHGoals: $("#teleop_h_goals").val(),
+		teleopLGoals: $("#teleop_l_goals").val()
+	}
 
-	var teamNum = $("#team_num").val();
-	var teamAlliance = $("#team_alliance").val();
-
-	var autoDefense = $("#auto_defense").val();
-	var autoSpeed = $("#auto_speed").val();
-	var autoHGoals = $("#auto_h_goals").val();
-	var autoLGoals = $("#auto_l_goals").val();
-
-	var teleopDefense = $("#teleop_defense").val();
-	var teleopSpeed = $("#teleop_speed").val();
-	var teleopHGoals = $("#teleop_h_goals").val();
-	var teleopLGoals = $("#teleop_l_goals").val();
-
-	console.log("stage", stage);
-	console.log("match_num", matchNum);
-
-	console.log("team_num", teamNum);
-	console.log("team_alliance", teamAlliance);
-
-	console.log("auto_defense", autoDefense);
-	console.log("auto_speed", autoSpeed);
-	console.log("auto_h_goals", autoHGoals);
-	console.log("auto_l_goals", autoLGoals);
-
-	console.log("teleop_defense", teleopDefense);
-	console.log("teleop_speed", teleopSpeed);
-	console.log("teleop_h_goals", teleopHGoals);
-	console.log("teleop_l_goals", teleopLGoals);
+	console.log("data", data);
 }
