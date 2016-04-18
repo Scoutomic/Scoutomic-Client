e2070af8d191a7e8a6c7fc8ecf00c805785ade88
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-04-17 18:27:36 -0700

    Make form save data to local database

--- app/scouting/form-manager.js
+++ app/scouting/form-manager.js
@@ -1,22 +1,34 @@
 var Datastore = require('nedb');
 
-var scoutingdb = new Datastore({ filename: __dirname + '/datastore/scouting.db', autoload: true });
+var localdb = new Datastore({ filename: __dirname + '/datastore/local.db', autoload: true });
 
 function submitScout() {
 	var data = {
-		stage: $("#stage").val(),
-		matchNum: $("#match_num").val(),
-		teamNum: $("#team_num").val(),
-		teamAlliance: $("#team_alliance").val(),
-		autoDefense: $("#auto_defense").val(),
-		autoSpeed: $("#auto_speed").val(),
-		autoHGoals: $("#auto_h_goals").val(),
-		autoLGoals: $("#auto_l_goals").val(),
-		teleopDefense: $("#teleop_defense").val(),
-		teleopSpeed: $("#teleop_speed").val(),
-		teleopHGoals: $("#teleop_h_goals").val(),
-		teleopLGoals: $("#teleop_l_goals").val()
+		time: Math.floor(Date.now() / 1000),
+		year: 2016,
+		fields: {
+			stage: $("#stage").val(),
+			matchNum: $("#match_num").val(),
+			teamNum: $("#team_num").val(),
+			teamAlliance: $("#team_alliance").val(),
+			autoDefense: $("#auto_defense").val(),
+			autoSpeed: $("#auto_speed").val(),
+			autoHGoals: $("#auto_h_goals").val(),
+			autoLGoals: $("#auto_l_goals").val(),
+			teleopDefense: $("#teleop_defense").val(),
+			teleopSpeed: $("#teleop_speed").val(),
+			teleopHGoals: $("#teleop_h_goals").val(),
+			teleopLGoals: $("#teleop_l_goals").val()
+		}
 	}
 
 	console.log("data", data);
+
+	localdb.insert(data, function(err, newDoc) {
+		if (err) {
+			console.log(err);
+		} else {
+			location.reload();
+		}
+	});
 }
