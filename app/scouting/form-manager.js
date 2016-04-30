2e9b9ec5b2b0c7176e876ec01af5117488ed86a1
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-04-29 23:41:42 -0700

    Update the sending of requests

--- app/scouting/form-manager.js
+++ app/scouting/form-manager.js
@@ -17,23 +17,28 @@ function submitScout() {
 	var data = {
 		time: Math.floor(Date.now() / 1000),
 		year: 2016,
+		event: "orfoo",
 		fields: {
 			stage: $("#stage").val(),
-			matchNum: $("#match_num").val(),
-			teamNum: $("#team_num").val(),
-			teamAlliance: $("#team_alliance").val(),
-			autoDefense: $("#auto_defense").val(),
-			autoSpeed: $("#auto_speed").val(),
-			autoHGoals: $("#auto_h_goals").val(),
-			autoLGoals: $("#auto_l_goals").val(),
-			teleopDefense: $("#teleop_defense").val(),
-			teleopSpeed: $("#teleop_speed").val(),
-			teleopHGoals: $("#teleop_h_goals").val(),
-			teleopLGoals: $("#teleop_l_goals").val()
+			match: $("#match_num").val(),
+			team: {
+				number: $("#team_num").val(),
+				alliance: $("#team_alliance").val(),
+			},
+			autonomous: {
+				defenses: $("#auto_defense").val(),
+				speed: $("#auto_speed").val(),
+				highGoals: $("#auto_h_goals").val(),
+				lowGoals: $("#auto_l_goals").val()
+			},
+			teleoperated: {
+				defenses: $("#teleop_defense").val(),
+				speed: $("#teleop_speed").val(),
+				highGoals: $("#teleop_h_goals").val(),
+				lowGoals: $("#teleop_l_goals").val()
+			}
 		}
 	};
-
-	console.log("data", data);
 
 	localdb.insert(data, function(err, newDoc) {
 		if (err) {
@@ -45,17 +50,17 @@ function submitScout() {
 }
 
 setInterval(function() {
-	var url = env.url ? env.url : "http://127.0.0.1";
+	var base = env.url ? env.url : "127.0.0.1";
 
 	localdb.find({}, function(err, docs) {
 		docs.forEach(function(doc) {
+			var url = genURL(base, ["scout", doc.year, doc.event, doc.fields.stage, doc.fields.match]);
+
 			$.ajax({
 				type: "POST",
 				url: url,
-				data: doc,
+				data: doc.fields,
 				success: function() {
-					console.log("successful request to", url);
-
 					localdb.remove({ _id: doc._id }, {}, function(err) {
 						if (err) {
 							console.log(err);
@@ -66,6 +71,18 @@ setInterval(function() {
 		});
 	});
 }, 5000);
+
+function genURL(base, path) {
+	if (!/^(f|ht)tps?:\/\//i.test(base)) {
+		base = "http://" + base;
+	}
+
+	if (!/\/$/i.test(base)) {
+		base = base + "/";
+	}
+
+	return base + path.join("/");
+}
 
 function _interopDefault (ex) {
 	return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex;
